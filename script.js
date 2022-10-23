0d618365c536978fdfa8c2a5b8af720c9b1bb969
No-E003 <96266089+No-E003@users.noreply.github.com>
2022-10-22 19:13:31 -0700

    Added a container to hold three buttons with the value displayed and gives the value back using a javascript alert

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
  //Global variables that will be used for the game
 let computerSelection = getComputerChoice();
-let playerSelection = 'paper';
 let computerScore = 0;
 let playerScore = 0;
 
@@ -11,7 +10,8 @@ function getComputerChoice() {
 };
 
 //function that stores the results for each round of the game depending on the player choice and computer random choice
-function playGame(playerSelection,computerSelection) {
+function playGame(playerSelection) {
+  let computerSelection = getComputerChoice();
   if(playerSelection == computerSelection) {
     return alert(`Its a draw. You chose ${playerSelection} and the computer chose ${computerSelection}. The current score between player and bot is ${playerScore} to ${computerScore}.`)
   } else if(playerSelection == 'rock' && computerSelection == 'scissors') {
@@ -29,22 +29,31 @@ function playGame(playerSelection,computerSelection) {
   }
 };
 
-function result() {
-  if(playerScore == computerScore) {
-    return alert(`No one wins! Final score: ${playerScore} to ${computerScore}`);
-  } else if(playerScore > computerScore) {
-    return alert(`You win! Final score: ${playerScore} to ${computerScore}`);
-  } else {
-    return alert(`The computer wins! Final score: ${playerScore} to ${computerScore}`);
-  }
-};
+//function result() {
+  //if(playerScore == computerScore) {
+    //return alert(`No one wins! Final score: ${playerScore} to ${computerScore}`);
+  //} else if(playerScore > computerScore) {
+    //return alert(`You win! Final score: ${playerScore} to ${computerScore}`);
+  //} else {
+    //return alert(`The computer wins! Final score: ${playerScore} to ${computerScore}`);
+  //}
+//};
 
  //Function that will store all the previous functions in to one single game in a series best of five matches
- function gamePlay() {
-  for( x = 0; x < 5; x++) {
-    let playerSelection = prompt("In a best of five against the computer. Choose between rock, paper, or scissors: ").toLowerCase();
-    let computerSelection = getComputerChoice();
-    playGame(playerSelection,computerSelection);
-  }
-  return result();
-};
+ //function gamePlay() {
+  //let playerSelection = prompt("In a best of five against the computer. Choose between rock, paper, or scissors: ").toLowerCase();
+  //let computerSelection = getComputerChoice();
+  //playGame(playerSelection,computerSelection);
+  //return result();
+//};
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click',() => {
+    alert(button.value);
+  });
+});
